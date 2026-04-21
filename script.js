565d42a6e3a6c0a19ad4ff9e13599e49d12c46c7
aryaanugrah <[aryadawuhan644@gmail.com]>
2026-04-21 11:09:07 +0700

    feat: tambah input umur dan perbaiki logika tampilan hasil di JS

--- script.js
+++ script.js
@@ -4,21 +4,23 @@ form.addEventListener('submit', function (e) {
     e.preventDefault();
     const nama = document.getElementById('nama').value;
     const email = document.getElementById('email').value;
+    const umur = document.getElementById('umur').value;
     const hp = document.getElementById('hp').value;
     const pesan = document.getElementById('pesan').value;
-    if (nama === "" || email === "" || hp === "" || pesan === "") {
+    if (nama === "" || email === "" || umur === "" || hp === "" || pesan === "") {
         alert("Semua field harus diisi!");
-        return;
         hasilPendaftaran.innerHTML = '<p>Silakan lengkapi semua field.</p>';
+        return;
     } if (!email.includes('@')) {
         alert("Email tidak valid!");
-        return;
         hasilPendaftaran.innerHTML = '<p>Silakan lengkapi semua field.</p>';
+        return;
     } hasilPendaftaran.innerHTML = ` <h2>Terima Kasih Telah Mendaftar! ${nama}</h2>
     <div style=" margin-top: 20px; padding: 20px; background-color: #f0f0f0; border-radius: 5px;">
     <h3>Data Pendaftaran Berhasil</h3>
     <p><strong>Nama:</strong> ${nama}</p>
     <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Umur:</strong> ${umur}</p>
     <p><strong>HP:</strong> ${hp}</p>
     <p><strong>Pesan:</strong> ${pesan}</p>
     </div>
